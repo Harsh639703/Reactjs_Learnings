a29dc2c5d53da9817ab29784098a2a2b47124460
Harsh639703 <harsh639703@gmail.com>
2024-05-18 21:57:59 +0530

    commiting my first React js app

---
+++ 01_basic_react_app/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Starting ReactJs journey, Jai shree Ganesh!</h1>
+  );
+}
+
+export default App;
---
+++ 02_vite_react_app/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Started Learning the ReactJs , Jai shree Shyam!</h1>
+    </>
+  )
+}
+
+export default App
