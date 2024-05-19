81c53a7e69afe5fa7abbfd93a06de04a26592bc1
Harsh639703 <harsh639703@gmail.com>
2024-05-19 15:19:39 +0530

    learned how to create new element in the vite jsx or how can we render our variable

--- 02_vite_react_app/src/App.jsx
+++ 02_vite_react_app/src/App.jsx
@@ -1,11 +1,12 @@
 import Champ from "./champ"
 function App() {
   
-
+const username = "Harsh Vishnoi"
   return (
        <>
        <Champ />
         <h1>Started Learning the ReactJs , Jai shree Shyam!</h1>
+        <h3>{username}</h3>
              
        </>
       
--- 02_vite_react_app/src/main.jsx
+++ 02_vite_react_app/src/main.jsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+const anotherelement=(
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+const another ="Vishnoi"
+const reactElement =React.createElement(//React provides the create element functionality
+  'a',
+  {href:'https://google.com' ,target:'_blank'},
+  'click me to go the google bhai',
+  another
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    // <App />
+    // anotherelement
+    reactElement
+   
 )
