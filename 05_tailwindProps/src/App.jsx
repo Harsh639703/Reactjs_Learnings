d014c5b9c41a39c65ab2eced98528947c18d303b
Harsh639703 <harsh639703@gmail.com>
2024-05-20 16:10:20 +0530

    Learned about the Tailwind , components in ReactJS and props in ReactJS

---
+++ 05_tailwindProps/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  
+  // let Objete={
+  //   name:"Harsh Vishnoi",
+  //   age:24
+  // }
+  return (
+    <>
+      <h1 className='bg-blue-400 text-black p-4 rounded-2xl mb-4'>I am Learning the Tailwind Test</h1> 
+       <Card username="Shyam" btnText="Click Me ->"/>
+       <Card username="Mohit" btnText="Click Here ->" />
+       
+    </>
+  )
+}
+
+export default App
+
+
+//devUI.io
