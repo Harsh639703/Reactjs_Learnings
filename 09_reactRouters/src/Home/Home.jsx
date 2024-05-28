9df4df3906358fa68d0dd9b2fb0b126140cf8696
Harsh639703 <harsh639703@gmail.com>
2024-05-28 10:10:08 +0530

    Some changes

--- 09_reactRouters/src/Home/Home.jsx
+++ 09_reactRouters/src/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 export default function Home() {
     return (
         <div className="mx-auto w-full max-w-7xl">
@@ -8,7 +8,7 @@ export default function Home() {
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
                         <h2 className="text-4xl font-bold sm:text-5xl">
                             Download Now
-                            <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
+                            <span className="hidden sm:block text-4xl">Harsh Vishnoi</span>
                         </h2>
 
                         <Link
--- 09_reactRouters/src/main.jsx
+++ 09_reactRouters/src/main.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
 
+import Home from './Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout />,
+    children:[
+      {
+        path:'/',
+        element:<Home />
+      },
+      {
+        path:'about',
+        element:<About />
+      },
+      {
+        path:'contact',
+        element:<Contact />
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
